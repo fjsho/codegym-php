3f0a81b0a2e9752a733d1363a2b0aceb17ee38f6
fjsho <gorimura7@gmail.com>
2021-05-13 21:33:20 +0900

    [fix]count($array)を変数に格納。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,9 +1,15 @@
 <?php
 $array = explode(',', $_GET['array']);
-
 // 修正はここから
-for ($i = count($array); $i > 0; $i--) {
-  for ($j = 0; $j < count($array) - 1; $j++) {
+$my_count = count($array);
+
+// function my_count($array) {
+//  print_r("call my_count\n");
+//   return count($array);
+// }
+
+for ($i = $my_count; $i > 0; $i--) {
+  for ($j = 0; $j < $my_count - 1; $j++) {
     if ($array[$j] > $array[$j + 1]) {
       $big = $array[$j];
       $array[$j] = $array[$j + 1];
@@ -12,6 +18,19 @@ for ($i = count($array); $i > 0; $i--) {
   }
 }
 // 修正はここまで
+
+//whileを使う場合
+// $i = 0;
+// while ($i < count($array)) {
+//   for ($j = 0; $j < count($array) - 1; $j++) {
+//     if ($array[$j] > $array[$j + 1]) {
+//       $big = $array[$j];
+//       $array[$j] = $array[$j + 1];
+//       $array[$j + 1] = $big;
+//     }
+//   }
+//   $i++;
+// }
 
 echo "<pre>";
 print_r($array);
